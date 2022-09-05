f1f4c6701e7c4200c47151c90fa27b9d0b4778d6
gopherlearning <gopherlearning@yandex.ru>
2022-09-05 21:52:45 +0300

    Fix args

--- internal/args.go
+++ internal/args.go
@@ -10,29 +10,29 @@ import (
 
 type ServerArgs struct {
 	Verbose            bool          `name:"verbose" short:"v" help:"Включить расширенное логирование" env:"VERBOSE"`
-	Config             string        `name:"config" short:"c" help:"Путь к файлу конфигурации" env:"CONFIG"`
-	ServerAddr         string        `mapstructure:"address,omitempty" short:"a" help:"Server address" name:"address" env:"ADDRESS" default:"127.0.0.1:8080"`
-	StoreFile          string        `mapstructure:"store_file,omitempty" short:"f" help:"строка, имя файла, где хранятся значения (пустое значение — отключает функцию записи на диск)" env:"STORE_FILE"`
-	DatabaseDSN        string        `mapstructure:"database_dsn,omitempty" short:"d" help:"строка c адресом подключения к БД" env:"DATABASE_DSN"`
-	Key                string        `mapstructure:"key,omitempty" short:"k" help:"Ключ подписи" env:"KEY"`
-	Restore            bool          `mapstructure:"restore,omitempty" short:"r" help:"булево значение (true/false), определяющее, загружать или нет начальные значения из указанного файла при старте сервера" env:"RESTORE"  default:"true"`
-	UsePprof           bool          `mapstructure:"-" help:"Использовать Pprof" env:"PPROF"`
-	ShowStore          bool          `mapstructure:"-" help:"Переодически выводить содержимое в консоль"`
-	StoreInterval      time.Duration `mapstructure:"store_interval,omitempty" short:"i" help:"интервал времени в секундах, по истечении которого текущие показания сервера сбрасываются на диск (значение 0 — делает запись синхронной)" env:"STORE_INTERVAL"  default:"400s"`
-	GenerateCryptoKeys bool          `mapstructure:"-" help:"Сгенерировать ключи для ассиметричного шифрования"`
-	CryptoKey          string        `mapstructure:"crypto_key,omitempty" help:"Путь к файлу, где хранятся приватный ключ шифрования" env:"CRYPTO_KEY"`
+	Config             string        `name:"config" json:"-" short:"c" help:"Путь к файлу конфигурации" env:"CONFIG"`
+	ServerAddr         string        `name:"address" short:"a" help:"Server address" env:"ADDRESS" default:"127.0.0.1:8080"`
+	StoreFile          string        `name:"store-file" json:"store_file" short:"f" help:"строка, имя файла, где хранятся значения (пустое значение — отключает функцию записи на диск)" env:"STORE_FILE"`
+	DatabaseDSN        string        `name:"database-dsn" json:"database_dsn" short:"d" help:"строка c адресом подключения к БД" env:"DATABASE_DSN"`
+	Key                string        `name:"key" short:"k" help:"Ключ подписи" env:"KEY"`
+	Restore            bool          `name:"restore" short:"r" help:"булево значение (true/false), определяющее, загружать или нет начальные значения из указанного файла при старте сервера" env:"RESTORE"  default:"true"`
+	UsePprof           bool          `help:"Использовать Pprof" env:"PPROF"`
+	ShowStore          bool          `help:"Переодически выводить содержимое в консоль"`
+	StoreInterval      time.Duration `name:"store-interval" json:"store_interval" short:"i" help:"интервал времени в секундах, по истечении которого текущие показания сервера сбрасываются на диск (значение 0 — делает запись синхронной)" env:"STORE_INTERVAL"  default:"400s"`
+	GenerateCryptoKeys bool          `help:"Сгенерировать ключи для ассиметричного шифрования"`
+	CryptoKey          string        `name:"crypto-key" json:"crypto_key" help:"Путь к файлу, где хранятся приватный ключ шифрования" env:"CRYPTO_KEY"`
 }
 
 type AgentArgs struct {
-	Verbose        bool          `mapstructure:"verbose,omitempty" short:"v" help:"Включить расширенное логирование" env:"VERBOSE"`
-	Config         string        `mapstructure:"-" short:"c" help:"Путь к файлу конфигурации" name:"config" env:"CONFIG"`
-	ServerAddr     string        `mapstructure:"address,omitempty" short:"a" help:"Server address" name:"address" env:"ADDRESS" default:"127.0.0.1:8080"`
-	Key            string        `mapstructure:"key,omitempty" short:"k" help:"Ключ подписи" env:"KEY"`
-	Format         string        `mapstructure:"format,omitempty" short:"f" help:"Report format" env:"FORMAT"`
-	Batch          bool          `mapstructure:"batch,omitempty" short:"b" help:"Send batch mrtrics" env:"BATCH" default:"true"`
-	PollInterval   time.Duration `mapstructure:"poll_interval,omitempty" short:"p" help:"Poll interval" env:"POLL_INTERVAL" default:"2s"`
-	ReportInterval time.Duration `mapstructure:"report_interval,omitempty" short:"r" help:"Report interval" env:"REPORT_INTERVAL" default:"10s"`
-	CryptoKey      string        `mapstructure:"crypto_key,omitempty" help:"Путь к файлу, где хранятся публийчный ключ шифрования" env:"CRYPTO_KEY" default:""`
+	Verbose        bool          `name:"verbose" short:"v" help:"Включить расширенное логирование" env:"VERBOSE"`
+	Config         string        `name:"config" json:"-" short:"c" help:"Путь к файлу конфигурации" env:"CONFIG"`
+	ServerAddr     string        `name:"address" short:"a" help:"Server address" env:"ADDRESS" default:"127.0.0.1:8080"`
+	Key            string        `name:"key" short:"k" help:"Ключ подписи" env:"KEY"`
+	Format         string        `name:"format" short:"f" help:"Report format" env:"FORMAT"`
+	Batch          bool          `name:"batch" short:"b" help:"Send batch mrtrics" env:"BATCH" default:"true"`
+	PollInterval   time.Duration `name:"poll-interval" json:"poll_interval" short:"p" help:"Poll interval" env:"POLL_INTERVAL" default:"2s"`
+	ReportInterval time.Duration `name:"report-interval" json:"report_interval" short:"r" help:"Report interval" env:"REPORT_INTERVAL" default:"10s"`
+	CryptoKey      string        `name:"crypto-key" json:"crypto_key" help:"Путь к файлу, где хранятся публийчный ключ шифрования" env:"CRYPTO_KEY" default:""`
 }
 
 // ReadConfig задаёт стандартные значения, читает конфиг, проверяет переменное окружение и флаги
