0a662b6d4fa31923f3f0663eff72261daeafac3f
gopherlearning <gopherlearning@yandex.ru>
2022-09-30 15:44:02 +0300

    Fix static test

--- internal/agent/client_test.go
+++ internal/agent/client_test.go
@@ -180,8 +180,9 @@ func TestClientDo(t *testing.T) {
 		require.NotNil(t, client)
 		req, err := http.NewRequest(http.MethodGet, "", nil)
 		require.NoError(t, err)
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		assert.ErrorContains(t, err, "unsupported protocol scheme")
+		defer resp.Body.Close()
 	})
 	t.Run("c.key == nil", func(t *testing.T) {
 		client, err := NewClient(context.TODO(), &internal.AgentArgs{Transport: "http"})
@@ -189,8 +190,9 @@ func TestClientDo(t *testing.T) {
 		require.NotNil(t, client)
 		req, err := http.NewRequest(http.MethodPost, "", nil)
 		require.NoError(t, err)
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		assert.ErrorContains(t, err, "unsupported protocol scheme")
+		defer resp.Body.Close()
 	})
 	t.Run("success request encrypted", func(t *testing.T) {
 		// generate a test server so we can capture and inspect the request
@@ -221,8 +223,9 @@ func TestClientDo(t *testing.T) {
 		require.NotNil(t, client)
 		req, err := http.NewRequest(http.MethodPost, "", bytes.NewBufferString("test message"))
 		require.NoError(t, err)
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		require.Error(t, err)
+		defer resp.Body.Close()
 	})
 	t.Run("error EncryptOAEP", func(t *testing.T) {
 		testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -234,8 +237,9 @@ func TestClientDo(t *testing.T) {
 		req, err := http.NewRequest(http.MethodPost, testServer.URL, bytes.NewBufferString("test message"))
 		require.NoError(t, err)
 		client.key.E = 1
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		require.Error(t, err)
+		defer resp.Body.Close()
 	})
 	t.Run("error encrypt bufer write", func(t *testing.T) {
 		testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -248,8 +252,9 @@ func TestClientDo(t *testing.T) {
 		require.NotNil(t, client)
 		req, err := http.NewRequest(http.MethodPost, testServer.URL, bytes.NewBufferString("test message"))
 		require.NoError(t, err)
-		_, err = client.Do(req)
+		resp, err := client.Do(req)
 		require.ErrorContains(t, err, "buffer wryte")
+		defer resp.Body.Close()
 	})
 }
 
